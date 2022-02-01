c9879f5fb9bba71f8be11299b6fb757eb27f2a9b
Zane Barker <Braz3n@users.noreply.github.com>
2022-02-01 22:23:11 +0900

    Updated opcodes.

--- constants.vhdl
+++ constants.vhdl
@@ -11,24 +11,25 @@ package constants is
 
     -- Instruction Constants
     constant opCodeNOP          : std_logic_vector(opCodeWidth-1 downto 0) := "000000";
-    -- Register Control
-    constant opCodeGET          : std_logic_vector(opCodeWidth-1 downto 0) := "000001";
-    constant opCodeSET          : std_logic_vector(opCodeWidth-1 downto 0) := "000010";
-    constant opCodeSETL         : std_logic_vector(opCodeWidth-1 downto 0) := "000011";
-    -- Program Counter Control
-    constant opCodeGOTOIFD0     : std_logic_vector(opCodeWidth-1 downto 0) := "000100";
-    constant opCodeGOTOIFD1     : std_logic_vector(opCodeWidth-1 downto 0) := "000101";
-    constant opCodeGOTO         : std_logic_vector(opCodeWidth-1 downto 0) := "000110";
-    -- Maths
-    constant opCodeADD          : std_logic_vector(opCodeWidth-1 downto 0) := "000111";
-    constant opCodeINC0         : std_logic_vector(opCodeWidth-1 downto 0) := "001000";
-    constant opCodeINC1         : std_logic_vector(opCodeWidth-1 downto 0) := "001001";
-    constant opCodeDEC0         : std_logic_vector(opCodeWidth-1 downto 0) := "001010";
-    constant opCodeDEC1         : std_logic_vector(opCodeWidth-1 downto 0) := "001011";
-    constant opCodeSUB          : std_logic_vector(opCodeWidth-1 downto 0) := "001100";
-    -- Logic
-    constant opCodeAND          : std_logic_vector(opCodeWidth-1 downto 0) := "001101";
-    constant opCodeOR           : std_logic_vector(opCodeWidth-1 downto 0) := "001110";
+    -- Register Control - 00xxxx
+    constant opCodeGET0         : std_logic_vector(opCodeWidth-1 downto 0) := "000001";
+    constant opCodeGET1         : std_logic_vector(opCodeWidth-1 downto 0) := "000010";
+    constant opCodeSET          : std_logic_vector(opCodeWidth-1 downto 0) := "000011";
+    constant opCodeSETL         : std_logic_vector(opCodeWidth-1 downto 0) := "000100";
+    -- Program Counter Control - 01xxxx
+    constant opCodeGOTOIFD0     : std_logic_vector(opCodeWidth-1 downto 0) := "010000";
+    constant opCodeGOTOIFD1     : std_logic_vector(opCodeWidth-1 downto 0) := "010001";
+    constant opCodeGOTO         : std_logic_vector(opCodeWidth-1 downto 0) := "010010";
+    -- Maths - 10xxxx
+    constant opCodeADD          : std_logic_vector(opCodeWidth-1 downto 0) := "100000";
+    constant opCodeINC0         : std_logic_vector(opCodeWidth-1 downto 0) := "100001";
+    constant opCodeINC1         : std_logic_vector(opCodeWidth-1 downto 0) := "100010";
+    constant opCodeDEC0         : std_logic_vector(opCodeWidth-1 downto 0) := "100011";
+    constant opCodeDEC1         : std_logic_vector(opCodeWidth-1 downto 0) := "100100";
+    constant opCodeSUB          : std_logic_vector(opCodeWidth-1 downto 0) := "100101";
+    -- Logic - 11xxxx
+    constant opCodeAND          : std_logic_vector(opCodeWidth-1 downto 0) := "110000";
+    constant opCodeOR           : std_logic_vector(opCodeWidth-1 downto 0) := "110001";
 end constants;
 
 package body constants is
